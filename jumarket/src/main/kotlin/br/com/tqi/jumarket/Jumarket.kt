133d90162d080410ff2c81cb8e064e96cef400dd
Debora <silvadeborafisio@gmail.com>
2023-07-29 01:22:45 -0300

    Uploading projet to repository

---
+++ jumarket/src/main/kotlin/br/com/tqi/jumarket/Jumarket.kt
@@ -0,0 +1,11 @@
+package br.com.tqi.jumarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Jumarket
+
+fun main(args: Array<String>) {
+	runApplication<Jumarket>(*args)
+}
---
+++ jumarket/src/test/kotlin/br/com/tqi/jumarket/JumarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.tqi.jumarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JumarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
